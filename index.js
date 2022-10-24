589df4e2328acd73dd0ae67d4a40e97a58f78134
sartajdev007 <sartaj5akik@gmail.com>
2022-10-24 15:32:36 +0600

    news add

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const { request } = require('express');
+const news = require('./data/news.json')
+
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -13,6 +16,31 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(categories);
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id)
+        res.send(category_news)
+    }
+
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    // console.log(req.params.id)
+    res.send(selectedNews)
+})
+
+
 
 app.listen(port, () => {
     console.log('server running', port)
